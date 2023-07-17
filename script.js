4bac17baaa0b0e827ed3f0ae8848aa6d942c2f61
RamaDias123 <Nurdias45@gmail.com>
2023-07-17 20:41:16 +0700

    update

--- script.js
+++ script.js
@@ -33,21 +33,6 @@ showHide.addEventListener('click', () => {
     }
 });
 
-// QnA
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("act-co");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
 // Form chat messages
 function openForm() {
   document.getElementById("myForm").style.display = "block";
